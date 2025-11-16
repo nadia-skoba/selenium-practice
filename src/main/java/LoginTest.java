d7975107bef4623cafef8551e1377a44a1a62d61
Надя <skobiukanada@gmail.com>
2025-11-16 18:39:06 +0200

    dz done 39

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -2,26 +2,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest {
-
-    public static void main(String[] args) {
-
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://www.saucedemo.com/");
-
-        String expectedText = "Swag Labs";
-        String actualTitle = driver.getTitle();
-
-        System.out.println("Фактичний заголовок: " + actualTitle);
-
-        if (actualTitle.contains(expectedText)) {
-            System.out.println(" Перевірка успішно виконена.");
-        } else {
-            System.out.println("FAil");
+    public class LoginTest {
+        public static void main(String[] args) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.saucedemo.com/");
+            String expectedText = "Swag Labs";
+            String actualTitle = driver.getTitle();
+            if (actualTitle.contains(expectedText)) {
+                System.out.println("Pass.");
+            } else {
+                System.out.println("Feil.");
+            }
+            driver.quit();
         }
-
-        driver.quit();
     }
-}
+
